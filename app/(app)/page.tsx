27e614f5ea28c17c77c10dc493e03ae33a22af2e
Sai Nihal <sainihaldiddi2002@gmail.com>
2024-05-20 22:47:31 -0700

    first commit

---
+++ app/(app)/page.tsx
@@ -0,0 +1,8 @@
+import { pages } from 'next/dist/build/templates/app-page';
+import React from 'react';
+
+function ChatPage(){
+    return <div>ChatPage</div>
+}
+
+export default ChatPage
---
+++ app/(app)/profile/page.tsx
@@ -0,0 +1,8 @@
+import { pages } from 'next/dist/build/templates/app-page';
+import React from 'react';
+
+function ProfilePage(){
+    return <div>ProfilePage</div>
+}
+
+export default ProfilePage
